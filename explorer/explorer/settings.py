4bc077d996b3b9c733fa90ba204f377f00d4262a
= <zainr7989@gmail.com>
2019-11-18 11:36:00 -0800

    [ADD] Model class for Note.

--- explorer/explorer/settings.py
+++ explorer/explorer/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'notes',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# note settings
+NOTE_TITLE_MAX_LENGTH = 600

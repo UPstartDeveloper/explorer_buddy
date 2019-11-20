78a5e7471c00b5c6db730f296276683a8d9cdab6
= <zainr7989@gmail.com>
2019-11-19 19:54:33 -0800

    [ADD] Dummy data to list view of Note instances.

--- explorer/notes/views.py
+++ explorer/notes/views.py
@@ -7,3 +7,10 @@ class NoteList(ListView):
     '''Renders a list of all Notes.'''
     model = Note
     template_name = 'notes/notebook.html'
+
+    def get(self, request):
+        ''' Get a list of all notes currently in the database.'''
+        notes = self.get_queryset().all()
+        return render(request, self.template_name, {
+            'notes': notes
+        })

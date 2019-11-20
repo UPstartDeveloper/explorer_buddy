0b8d1e0b2ddba6e1b5fe8cbed90cdb7a5981c450
= <zainr7989@gmail.com>
2019-11-19 18:58:12 -0800

    [PROTECT] secret key.

--- explorer/explorer/settings.py
+++ explorer/explorer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x(#8e5wqh9_418tjt*c5s6k3#%)=#8zmqf44s6@_-cgy_7kczc'
+load_dotenv()
+SECRET_KEY = os.getenv('SECURITY_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

2e55157ff3c18cf905d46d60df92c0a7cb0abb66
= <zainr7989@gmail.com>
2019-11-20 09:05:08 -0800

    [ADD] Empty classes for the main pages.

--- explorer/notes/urls.py
+++ explorer/notes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from notes.views import NoteList
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'notes'
 urlpatterns = [
         path('', NoteList.as_view(), name='notes-list-page'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- explorer/notes/views.py
+++ explorer/notes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic.list import (
+    ListView, DetailView, CreateView, DeleteView, UpdateView)
 from notes.models import Note
 
 
@@ -14,3 +15,23 @@ class NoteList(ListView):
         return render(request, self.template_name, {
             'notes': notes
         })
+
+
+class NoteDetail(DetailView):
+    '''Display the information currently on one Note.'''
+    pass
+
+
+class NoteCreate(CreateView):
+    '''Render a form to create new note.'''
+    pass
+
+
+class NoteUpdate(UpdateView):
+    '''Render a form to edit a note.'''
+    pass
+
+
+class NoteDelete(DeleteView):
+    '''Render a form for user to delete a Note.'''
+    pass

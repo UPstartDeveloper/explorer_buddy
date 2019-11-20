9e53e8d0248cfb1f0e81995bbc9ef73275a3346d
= <zainr7989@gmail.com>
2019-11-19 19:42:07 -0800

    [ADD] Configure admin site.

--- explorer/explorer/settings.py
+++ explorer/explorer/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'notes',
-    'accounts',
     'notes.apps.NotesConfig',
     'accounts.apps.AccountsConfig',
 ]

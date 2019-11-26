e18ee4f8548781bd96afdd42c1c4f8afe30707e6
= <zainr7989@gmail.com>
2019-11-26 14:41:06 -0800

    [RESTORE] Settings to previous values.

--- explorer/explorer/settings.py
+++ explorer/explorer/settings.py
@@ -138,10 +138,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 # mock live cdn i.e. AWS S3
-LOCAL_CDN_PATH = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
-STATIC_ROOT = os.path.join(LOCAL_CDN_PATH, 'static')
+# LOCAL_CDN_PATH = os.path.join(os.path.dirname(BASE_DIR), 'static_cdn')
+# STATIC_ROOT = os.path.join(LOCAL_CDN_PATH, 'static')
+
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static'),  # now represents the local version
@@ -150,4 +152,4 @@ STATICFILES_DIRS = [
 # used to render images from ImageField in Note model
 # credit goes to: Justin Mitchel at https://tinyurl.com/vofmamq
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(LOCAL_CDN_PATH, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

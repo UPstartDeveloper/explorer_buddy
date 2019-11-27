f7323c7aceeb136030e15b83ac0f1d81837b1208
= <zainr7989@gmail.com>
2019-11-27 13:27:48 -0800

    [ADD] Setup method for tests.

--- explorer/notes/tests.py
+++ explorer/notes/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.test.client import RequestFactory
+from django.contrib.auth.models import User
+from notes.models import Note
+from django.urls import reverse
+from notes.views import NoteCreate
 
-# Create your tests here.
+
+class NoteCreateTests(TestCase):
+    '''A user can insert a new note into the database.'''
+    def setUp(self):
+        '''Instaniate RequestFactory and User to make POST requests.'''
+        self.factory = RequestFactory()
+        self.user = User.objects.create(username='Zain',
+                                        email='zain_14@icloud.com',
+                                        password='bismillah')

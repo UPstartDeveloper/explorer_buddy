251b82ac905bd5618ecb246feff15326a6ecbf32
= <zainr7989@gmail.com>
2019-11-27 19:07:57 -0800

    [UPDATE] Signup tests to match updated fields.

--- explorer/accounts/tests.py
+++ explorer/accounts/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from accounts.views import SignUpView
+from accounts.views import signup
 from django.contrib.auth.models import User, AnonymousUser
 from django.test.client import RequestFactory
 
@@ -16,13 +16,14 @@ class SignUpViewTests(TestCase):
         # the anonymous user can visit the signup page
         get_request = self.factory.get('accounts:signup-form')
         get_request.user = self.unknown_user
-        response = SignUpView.as_view()(get_request)
+        response = signup(get_request)
         # test that the form looks correct
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Sign Up')
 
         # simulate user making a new account with a username and password
         form_data = {
+            'email': 'zain_14@icloud.com',
             'username': 'LittleEinstein',
             'password1': 'Sc1ence_R0cks!',
             'password2': 'Sc1ence_R0cks!'
@@ -30,8 +31,9 @@ class SignUpViewTests(TestCase):
 
         post_request = self.factory.post('accounts/signup/', form_data)
         post_request.user = self.unknown_user
-        response = SignUpView.as_view()(post_request)
+        response = signup(post_request)
         # test the view and the User accounts after form submission
         self.assertEqual(response.status_code, 302)
         user = User.objects.get(username=form_data['username'])
-        assert user is not None
+        self.assertNotEqual(user, None)
+        self.assertEqual(user.email, form_data['email'])

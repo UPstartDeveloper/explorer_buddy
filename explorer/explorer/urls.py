96b23d8a2b73008bd543682616e43e36917a89c4
= <zainr7989@gmail.com>
2019-11-28 09:22:17 -0800

    [ADD] URL pattern for the password reset done view.

--- explorer/explorer/urls.py
+++ explorer/explorer/urls.py
@@ -34,6 +34,9 @@ urlpatterns = [
          auth_views.PasswordResetView.as_view(
                     template_name='accounts/password_reset.html'),
          name='password_reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(
+                template_name='accounts/password_reset_done.html'),
+         name='password_reset_done'),
 
 ]
 

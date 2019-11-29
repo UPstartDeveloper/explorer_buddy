694d85512db92af970a10da0cb52d1817d3181cf
= <zainr7989@gmail.com>
2019-11-28 16:17:12 -0800

    [FIX] Modularize accounts views.

--- explorer/accounts/urls.py
+++ explorer/accounts/urls.py
@@ -3,12 +3,30 @@ from django.urls import path, include
 from accounts.views import signup
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
+from accounts.views import PasswordResetConfirm, PasswordResetView
 
 
 app_name = 'accounts'
 urlpatterns = [
     path('signup/', signup, name='signup-form'),
     path('',
-         auth_views.LoginView.as_view(template_name="registration/login.html"),
+         auth_views.LoginView.as_view(template_name="accounts/login.html"),
          name='login'),
+    # views for resetting the password
+    path('password-reset/',
+         PasswordResetView.as_view(
+                    template_name='accounts/password_reset.html'),
+         name='password_reset'),
+
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+          template_name='accounts/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+         PasswordResetConfirm.as_view(), name='password_reset_confirm'),
+
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+                    template_name='accounts/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
--- explorer/accounts/views.py
+++ explorer/accounts/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
+import django.contrib.auth.views as auth_views
+from django.urls import reverse, reverse_lazy
 
 # Create your views here.
 
@@ -19,3 +21,14 @@ def signup(request):
         form = ExplorerSignUpForm()
         context = {'form': form}
         return render(request, 'accounts/signup.html', context)
+
+
+class PasswordResetView(auth_views.PasswordResetView):
+    '''Emails user with a link to reset their password.'''
+    success_url = reverse_lazy('accounts:password_reset_done')
+
+
+class PasswordResetConfirm(auth_views.PasswordResetConfirmView):
+    '''Presents the form for entering a new password.'''
+    success_url = reverse_lazy('accounts:password_reset_complete')
+    template_name = 'accounts/password_reset_confirm.html'

f9d09ab08cd3bb08f04e76f87926f74ee3d32825
= <zainr7989@gmail.com>
2019-11-30 13:15:33 -0800

    [ADD] View to read, edit, or delete single Note.

--- explorer/api/urls.py
+++ explorer/api/urls.py
@@ -3,5 +3,7 @@ import api.views as api_views
 
 app_name = 'api'
 urlpatterns = [
-    path('notes/', api_views.NoteListOrCreateView.as_view(), name='notebook'),
+    path('notes/', api_views.NoteListOrCreate.as_view(), name='notebook'),
+    path('notes/<int:pk>/', api_views.NoteInspectionTool.as_view(),
+         name='note-tool'),
 ]
--- explorer/api/views.py
+++ explorer/api/views.py
@@ -5,10 +5,19 @@ from notes.models import Note
 from api.serializers import NoteSerializer
 
 
-class NoteListOrCreateView(ListCreateAPIView):
+class NoteListOrCreate(ListCreateAPIView):
     """Presents a list of all Note instances.
        Allows for the creation of single Note.
        Method handlers for GET and POST.
+    """
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
+
+
+class NoteInspectionTool(RetrieveUpdateDestroyAPIView):
+    """User is able to read, update, or delete a Note instance.
+       Lookup uses the id field of the specific Note object.
+        Work with GET, PUT, PATCH, or DELETE methods.
     """
     queryset = Note.objects.all()
     serializer_class = NoteSerializer

677ea71469f94d12174db01e28987b9380dde09e
= <zainr7989@gmail.com>
2019-11-30 14:04:15 -0800

    [ADD] Test retrieval of single Note in the NoteInspectionTool view.

--- explorer/api/tests.py
+++ explorer/api/tests.py
@@ -4,7 +4,7 @@ from rest_framework.test import (
 )
 from rest_framework import status
 from django.contrib.auth.models import User
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from notes.models import Note
 from api.serializers import NoteSerializer
 import api.views as api_views
@@ -24,10 +24,33 @@ class NoteListOrCreateTests(APITestCase):
     def test_create_note(self):
         '''A user is able to create a single Note from this view.'''
         create_url = reverse('api:notebook')
-        data = {'title': "Frogs",
+        data = {'title': 'Test Note',
                 'author': self.user.id,
-                'content': 'An animal able to swim well and jump high.'}
+                'content': 'This is a test note.'}
         post_request = self.factory.post(create_url, data)
         # test the Note is then created
         response = self.view(post_request)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class NoteInspectionToolTests(APITestCase):
+    def setUp(self):
+        """Instance variables needed to test retrieving, editing,
+           or deleting a Note instance.
+        """
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create(username="Zain",
+                                        email='zainr7989@gmail.com',
+                                        password='Fake_passwords_for_days')
+        self.view = api_views.NoteListOrCreate.as_view()
+        self.note = Note.objects.create(title='Test Note',
+                                        author=self.user,
+                                        content='This is a test note.')
+
+    def test_retrieve_note(self):
+        '''A user is able to view a Note instance by its unique id.'''
+        read_url = reverse('api:note-tool', args=[self.note.id])
+        get_request = self.factory.get(read_url)
+        # test the JSON object is found
+        response = self.view(get_request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

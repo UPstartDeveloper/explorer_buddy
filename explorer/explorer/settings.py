071cb7772c543cfd735cb98760861808634a4f56
= <zainr7989@gmail.com>
2019-12-02 10:33:16 -0800

    [ADD] Block for navbar.

--- explorer/explorer/settings.py
+++ explorer/explorer/settings.py
@@ -65,7 +65,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            # project templatws
+            # project templates
             os.path.join(BASE_DIR, 'templates').replace('\\', '/'),
         ],
         'APP_DIRS': True,

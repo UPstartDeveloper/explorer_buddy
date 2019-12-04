e1e76c3730d44b4fbf943c061dddade78444bf9b
= <zainr7989@gmail.com>
2019-12-04 09:55:38 -0800

    [ADD] Profile model.

--- explorer/accounts/models.py
+++ explorer/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT,)
+    mugshot = models.ImageField(upload_to='image/',
+                                blank=True,
+                                help_text="User profile image")
+
+    def __str__(self):
+        '''Return the related User's username.'''
+        return self.user.username

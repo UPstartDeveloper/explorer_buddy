502f1c70c8c07a7e2e0f5b1ee739d7d3d2e08503
= <zainr7989@gmail.com>
2019-12-08 13:54:43 -0800

    [UPDATE] Re-add Profile model.

--- explorer/accounts/models.py
+++ explorer/accounts/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from explorer import settings
 
-"""
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT,)
-    mugshot = models.ImageField(upload_to='image/',
-                                blank=True,
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    mugshot = models.ImageField(upload_to='images/',
+                                default=(
+                                    settings.STATICFILES_DIRS[0] +
+                                    'images/user-icon.png'),
                                 help_text="User profile image")
 
     def create_profile(sender, **kwargs):
         '''Instanitate a Profile for each new user who signs up.'''
         if kwargs['created'] is not None:
             profile = (
-            Profile.objects.create(user=kwargs['accounts_profile.user_id'])
-        )
+                Profile.objects.create(user=kwargs['accounts_profile.user_id'])
+            )
 
     post_save.connect(create_profile, sender=User)
 
     def __str__(self):
         '''Return the related User's username.'''
-        return self.user.username
-
-"""
+        return f"{self.user.username}'s Profile"

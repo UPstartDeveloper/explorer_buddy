7899118c3d3fbe77ac6fc840f655626889baa6b6
= <zainr7989@gmail.com>
2019-12-15 18:46:52 -0500

    [ADD] URL pattern for Medium view.

--- explorer/notes/urls.py
+++ explorer/notes/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from notes.views import (
-    NoteList, NoteDetail, NoteCreate, NoteUpdate, NoteDelete)
+    NoteList, NoteDetail, NoteCreate, NoteUpdate, NoteDelete, post_on_medium)
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -11,4 +11,11 @@ urlpatterns = ([
         path('<slug:slug>/edit/', NoteUpdate.as_view(), name="edit_note_form"),
         path('<slug:slug>/delete/', NoteDelete.as_view(), name='delete_note'),
         path('<slug:slug>/', NoteDetail.as_view(), name='notes-detail-page'),
+        path((
+            'https://medium.com/m/oauth/authorize?' +
+            'client_id={{clientId}}' +
+            '&scope=publishPost&state={{state}}' +
+            '&response_type=code&redirect_uri={{redirectUri}}'),
+            post_on_medium, name='post-to-medium'
+        ),
 ])

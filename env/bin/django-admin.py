76be44bce9e31b34ab03e804155cad3dbbf0d15c
= <zainr7989@gmail.com>
2020-08-01 11:08:57 -0400

    Remove virtual environment from git.

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/zainraza/Downloads/dev/courses/Year_1_Term1_3/explorer_buddy/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
